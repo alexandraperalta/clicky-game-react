47f45465ce536bb662e647924f52fe9ae45ac896
AlexPeralta <alexandra.peralta1@gmail.com>
2018-06-24 00:20:10 -0400

    formatting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import CharCard from "./components/CharCard";
-import logo from './logo.svg';
 import './App.css';
 import friends from './friends.json';
 import Header from "./components/Header";
@@ -61,27 +60,24 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">      
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro"/>      
+      <div className="App">    
         <Header
         score={this.state.score}
         highScore={this.state.highScore}
           />
-          {this.state.friends.map(friend => (
-            <CharCard
-            clickedFriend={this.clickedFriend}
-              id={friend.id}
-              key={friend.id}
-              name={friend.name}
-              image={friend.image}
-              occupation={friend.occupation}
-              location={friend.location}
-            />
-          ))}
+          <div className="container">
+            {this.state.friends.map(friend => (
+              <CharCard
+                clickedFriend={this.clickedFriend}
+                id={friend.id}
+                key={friend.id}
+                name={friend.name}
+                image={friend.image}
+                occupation={friend.occupation}
+                location={friend.location}
+              />
+            ))}
+          </div>
       </div>
 
     );
--- src/components/CharCard/CharCard.js
+++ src/components/CharCard/CharCard.js
@@ -2,26 +2,10 @@ import React from "react";
 import "./CharCard.css";
 
 const CharCard = props => (
-  <div className="card">
+  <div className="card" onClick={() => props.clickedFriend(props.id)}>
     <div className="img-container">
       <img alt={props.name} src={props.image} />
     </div>
-    <div className="content">
-      <ul>
-        <li>
-          <strong>Name:</strong> {props.name}
-        </li>
-        <li>
-          <strong>Occupation:</strong> {props.occupation}
-        </li>
-        <li>
-          <strong>Location:</strong> {props.location}
-        </li>
-      </ul>
-    </div>
-    <span onClick={() => props.clickedFriend(props.id)} className="remove">
-      𝘅
-    </span>
   </div>
 );
 
